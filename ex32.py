3ebd241cce5e2b50c0707766e2873159f5a37daa
ilonger <ilonger@outlook.com>
2018-01-23 10:39:40 +0800

    xinzeng

--- ex32.py
+++ ex32.py
@@ -18,4 +18,13 @@ for i in change:
 #we can also build lists,first start with an empty one
 elements = []
 
-#
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print("Adding %d to the list." %i)
+    # append id a function that lists understand
+    elements.append(i)
+
+#now we can print them out too
+for i in elements:
+    print("Element was : %d" %i)
+
