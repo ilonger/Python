c384475644b453f4b519a2baff1d4e1b31846843
ilonger <ilonger@outlook.com>
2018-01-24 21:16:16 +0800

    附加题：将这个 while 循环改成一个函数，将测试条件(i < 6)中的 6 换成一个变量

--- ex33.py
+++ ex33.py
@@ -1,15 +1,20 @@
 i = 0
 numbers = []
+maxium = int(input("please input a number!\n"))
+step = int(input("please input a number!\n"))
 
-while i < 6:
-    print("At the top i is %d" % i )
+while i < maxium:
+    print("At the top i is %d" % i)
     numbers.append(i)
 
     i = i + 1
-    print("Numbers now: ",numbers)
+    print("Numbers now: ", numbers)
     print("At the bottom i is %d" % i)
 
 print("The numbers:")
 
 for num in numbers:
     print(num)
+
+
+
